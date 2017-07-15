06825afffecff4172f560fef75cc5986c68d76d9
Kelsey Hayes <kelseyhayes@Kelseys-MBP.home>
2017-07-14 23:24:11 -0400

    added first bit of pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+/*Round 1 Question 1 Pseudocode
+-On page load, round1 div appears which holds the first question and its answers
+-Right and wrong answers are preselected
+-If user clicks right answer, they continue living
+-If user clicks wrong answer, one step closer to death

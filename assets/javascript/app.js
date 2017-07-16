97c98762eb1076cb94433c17b08ad2fec9630947
Kelsey Hayes <kelseyhayes@Kelseys-MBP.home>
2017-07-16 11:08:15 -0400

    added window on load function and timeout function, still have more to do

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,25 @@
 /*
-Round 1 Question 1 Pseudocode
--On page load, round1 div appears which holds the first question and its answers
--Right and wrong answers are preselected
--If user clicks right answer, they continue living
--If user clicks wrong answer, one step closer to death
 
-Question 1 function, on click, if user selects the right answer
-alert them that they are right, add a point to win score
-if user selects the wrong answer, alert them that they are wrong
-and add point to lose score
+only one answer per question
+saves responses
+when click done or time up
+shows score
 
-set event listeners for choices connecting to li divs in html, connect 
-those divs to a function through an event listener(on click event)
+on click start button
+start counter
 
-There is a previous button and a next button to toggle from one page
-to the next. 
-when user clicks next button, previous question dissapears
+stop counter
+if time runs out
+
+If click right answer
+push to right answers array
+add up sum of correct answers
+
+If click wrong answer 
+push to wrong answers array
+add up sum of incorrect answers
+
+if game time out or 
 
 */
 
@@ -23,14 +27,12 @@ when user clicks next button, previous question dissapears
 //This section is the code for my first question
 
 
-var questions = {}
 var wins = 0;
 var losses = 0;
 
-
 $(document).ready(function() {
 
-
+});
 $(".wrong").click(function(){
     losses++;
     $(".loss").html(losses);
@@ -43,17 +45,14 @@ $(".right").click(function(){
 })
 ;
 
-function nextquestion(){
-  var question1 = document.getElementByClassName('round1');
-  if (question1.style.display === 'none') {
-        question1.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
+$(window).load(function(){ 
+setTimeout(function(){ alert("Hello"); }, 3000);
+})
+/*function stopgame(){
+  alert("done!");
 }
 
-function changescore (){
+/*function changescore (){
+}*/
 
-}
 
-});

18abd909c675e0594a362ee0877286730cb70a44
Kelsey Hayes <kelseyhayes@Kelseys-MBP.home>
2017-07-16 18:11:41 -0400

    formatted title page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,3 @@
-/*
-
-only one answer per question
-saves responses
-when click done or time up
-shows score
-
-on click start button
-start counter
-
-stop counter
-if time runs out
-
-If click right answer
-push to right answers array
-add up sum of correct answers
-
-If click wrong answer 
-push to wrong answers array
-add up sum of incorrect answers
-
-if game time out or 
-
-*/
-
-
-//This section is the code for my first question
-
-
 var wins = 0;
 var losses = 0;
 var wrongAnswers = [];
@@ -52,43 +23,15 @@ $(".right").click(function(){
 })
 ;
 
-
-//see all numbers added to array
-
-
-
-
-
-//This code sets the timer function
-/*function startTimer(duration, display) {
-    var timer = duration, seconds;
-    setInterval(function () {
-        seconds = parseInt(timer % 60, 10);
-
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.text(seconds);
-
-        if (--timer < 0) {
-            timer = duration;
-        }
-
-    }, 1000);
-}*/
-
-/*jQuery(function ($) {
-    var oneminute = 60 * 5,
-        display = $('#time');
-    startTimer(oneminute, display);
-});*/
 //This code sets the
 $(window).load(function(){ 
-  //window.setTimeout(GameOver, 5000);
+ // window.setTimeout(GameOver, 30000);
 })
 
 function GameOver(){
 $('.right').prop('disabled', true);
 $('.wrong').prop('disabled', true);
+alert("GAME OVER");
 }
 
 
@@ -97,20 +40,17 @@ $('.wrong').prop('disabled', true);
 $("#finished").click(function(){
 $('.wrong').prop('disabled', true);
 $('.right').prop('disabled', true);
+alert("GAME OVER");
   })
 ;
-/*On page load
-set Game Over function at 30 seconds
-sets both right and wrong to disabled
- */
 
 
 /*functions for specific buttons, I know theres an easier way to do this, 
 I just haven't figured it out yet*/
 
 $(".wrong1").click(function(){
-$('.wrong1').prop('disabled', true);
-$('.right1').prop('disabled', true);
+$(".wrong1").prop('disabled', true);
+$(".right1").prop('disabled', true);
   })
 ;
 
@@ -127,6 +67,29 @@ $('.right3').prop('disabled', true);
   })
 ;
 
+$(".wrong4").click(function(){
+$('.wrong4').prop('disabled', true);
+$('.right4').prop('disabled', true);
+  })
+;
+
+$(".wrong4").click(function(){
+$('.wrong4').prop('disabled', true);
+$('.right4').prop('disabled', true);
+  })
+;
+
+$(".wrong5").click(function(){
+$('.wrong5').prop('disabled', true);
+$('.right5').prop('disabled', true);
+  })
+;
+
+$(".wrong6").click(function(){
+$('.wrong6').prop('disabled', true);
+$('.right6').prop('disabled', true);
+  })
+;
 
 $(".right1").click(function(){
 $('.right1').prop('disabled', true);
@@ -145,6 +108,31 @@ $('.right3').prop('disabled', true);
 $('.wrong3').prop('disabled', true);
   })
 ;
+
+$(".right4").click(function(){
+$('.right4').prop('disabled', true);
+$('.wrong4').prop('disabled', true);
+  })
+;
+
+$(".right4").click(function(){
+$('.right4').prop('disabled', true);
+$('.wrong4').prop('disabled', true);
+  })
+;
+
+$(".right5").click(function(){
+$('.right5').prop('disabled', true);
+$('.wrong5').prop('disabled', true);
+  })
+;
+
+$(".right6").click(function(){
+$('.right6').prop('disabled', true);
+$('.wrong6').prop('disabled', true);
+  })
+;
+
 /*function stopgame(){
   alert("done!");
 }
